bf643e3fbed6a70ea2c0d545bf1124026d26ea70
nkapzamalov <nkapzamalov@gmail.com>
2026-03-31 17:04:50 +0300

    switch DB config to mysql2 promise pool

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,15 @@
-import mysql from "mysql2";
+import 'dotenv/config';
+import mysql, { PoolOptions } from "mysql2/promise";
 
-const connection = mysql.createConnection({
+const access: PoolOptions = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+};
 
-export default connection;
+const pool =  mysql.createPool(access);
+
+export default pool;
