c19596f17c0fa24929db1ccb73422b4a2fca6b90
nkapzamalov <nkapzamalov@gmail.com>
2026-03-31 13:23:18 +0300

    setup mysql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import 'dotenv/config'
+import connection from "./config/db.js";
 import type { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port = 3000;
+const port = process.env.PORT;
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Hello World!");
